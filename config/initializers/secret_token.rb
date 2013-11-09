3008434bf1db15cbe31e35231a377a7212e0da06
(your git username) <(your git email)>
2013-11-09 22:04:51 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventTracker::Application.config.secret_token = '4b9422e899d8330d89a4903aa5e13f03ad0f9a97a251ccdc608709ef5c6ed146069d801424a4ab45b79cf6a2f167f88ccf805ff9fd245b6ff2560c38b8fb72de'
